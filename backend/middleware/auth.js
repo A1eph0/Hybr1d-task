624477ae0f734380e460c2079e19c32867d3614c
Hrishi Narayanan <68889529+hr1sh1-coding@users.noreply.github.com>
2022-05-22 11:46:23 +0530

    authentication middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const auth = (req, res, next) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.status(401).json({msg:"No Authentication! Access Denied!"})
+        
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.status(401).json({msg:"Verification Failed! Access Denied!"})
+
+        req.user = verified;
+        console.log("verified")
+        console.log(req.user)
+        next();
+    }catch (err) {
+        res.status(500).json('Error :'+ err.message)
+    }
+};
+
+module.exports = auth;
